284008b8be984ef2d507b2567da2f976ada2faaf
Konstantin <kostik-kostik-12@mail.ru>
2024-07-16 20:40:52 +0300

    add work

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./static')); // все файлы сайта будут тут
+
+const port = 3000;
+
+app.listen(port, () => {
+	console.log(`Сервер запущен на порту ${port}`);
+})
+
+// WebPack - можно сделать код кроссбраузерным
+// Vite
+// Gulp
